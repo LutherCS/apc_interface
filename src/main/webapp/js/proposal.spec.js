ed73df1714a00547c02359f33413f19e39b6375d
Unknown <couger01@luther.edu>
2017-11-18 16:34:58 -0600

    Controller Test partially working
    - partially implemented controller inject for each test
    - throws up paramsProvider error.

--- src/main/webapp/js/proposal.spec.js
+++ src/main/webapp/js/proposal.spec.js
@@ -1,30 +1,46 @@
 describe('Proposal controller', function() {
-    var $controller, $rootScope;
+    var module, $controller, $rootScope, proposalCtrl;
     beforeEach(function() {
-        angular.mock.module('CourseProposalApp');
+         module = angular.mock.module('CourseProposalApp');
     });
-    var $controller, $rootScope, $scope;
-    beforeEach(inject(function(_$controller_,_$rootScope_) {
+    var $controller, $rootScope, $scope, $log, $location, $routeParams, $filter, $window, params, dataSrv, userSrv, EVENTS;
+    beforeEach(inject(function(_$controller_,_$rootScope_,_$log_,_$location_,_$routeParams_,_$filter_,_$window_,_params_,_dataSrv_,_userSrv_,_EVENTS_) {
         $controller = _$controller_;
         $rootScope = _$rootScope_;
+        $scope = $rootScope.$new;
+        $log = _$log_;
+        $location = _$location_;
+        $routeParams = _$routeParams_;
+        $filter = _$filter_;
+        $window = _$window_;
+        params = _params_;
+        dataSrv = _dataSrv_;
+        userSrv = _userSrv_;
+        EVENTS = _EVENTS_;
+        
+        proposalCtrl = $controller('proposalCtrl', {
+            $rootScope : _$rootScope_,
+            $scope : $scope,
+            $log : _$log_,
+            $location : _$location_,
+            $routeParams : _$routeParams_,
+            $filter : _$filter_,
+            $window : _$window_,
+            params : _params_,
+            dataSrv : _dataSrv_,
+            userSrv : _userSrv_,
+            EVENTS : _EVENTS_
+        });
     }));
-    it('should be defined', function() {
-        expect($controller).toBeDefined();
-    });
-    it('has rootScope defined', function() {
-        expect($rootScope).toBeDefined();
-    });
-    it('has rootScope.$new defined', function() {
-        expect($rootScope.$new).toBeDefined();
-    });
 //    beforeEach(inject(function($controller,$rootScope, $scope, $log, $location, $routeParams, $filter, $windows, params, dataSrv, userSrv, EVENTS){
 //        controller = $controller('proposalCtrl', {$rootScope: $rootScope, $scope: $scope, $log: $log, $location: $location, $routeParams: $routeParams, $filter: $filter, $windows: $windows, params: params, dataSrv: dataSrv, userSrv: userSrv, EVENTS: EVENTS});
 //    }));
-//    describe('$scope.selectedDept', function() {
-//        it('checks that the department matches', function() {
-//            expect().toEqual("MUS");
-//        });
-//    });
+    describe('$scope.selectedDept', function() {
+        it('checks that the department matches', function() {
+            $scope.selectedDept = $filter("filter")($scope.depts, {abbrev : "MUS"},true)[0];
+            expect($scope.selectedDept.abbrev).toEqual("MUS");
+        });
+    });
     it('holds a fundamental truth', function(){
         expect(2 + 2).toEqual(4);
     });
