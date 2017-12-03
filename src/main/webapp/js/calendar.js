d21a52207140cf7a7a9deea2dd7e43bb4cb145ad
Joshua Jackson <jackjo06@luther.edu>
2017-12-02 19:34:12 -0600

    Added repeat functionality to template and retrieved information from database.

--- src/main/webapp/js/calendar.js
+++ src/main/webapp/js/calendar.js
@@ -8,7 +8,18 @@ var app = angular.module("CourseProposalApp");
 
 app.controller("calendarCtrl", calendarCtrl);
 
-calendarCtrl.$inject=["$rootScope", "$scope", "$filter", "$log", "$routeParams", "$location", "userSrv", "courseSrv", "archiveSrv"];
-function calendarCtrl($rootScope, $scope, $filter, $log, $routeParams, $location, userSrv, courseSrv, archiveSrv) {
-
+calendarCtrl.$inject=["$rootScope", "$scope", "$filter", "$log", "$routeParams", "$q", "$location", "userSrv", "dataSrv", "courseSrv", "archiveSrv"];
+function calendarCtrl($rootScope, $scope, $filter, $log, $routeParams, $q, $location, userSrv, dataSrv, courseSrv, archiveSrv) {
+    
+    $scope.retrievingData = true;
+    
+    initCalendar();
+    
+    function initCalendar() {
+        return $q.all([dataSrv.getEvents()]).then(function(data){
+            $scope.calendar = data[0]
+            $scope.retrievingData = false;
+            console.debug($scope.calendar)
+        });
+    }
 }
